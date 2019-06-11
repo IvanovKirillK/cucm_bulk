9cf7e2a9f42a9b61849cd2be0784dd4ff7a416ac
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-06-11 12:07:01 +0300

    site operator bug fix

--- tasks/get_operator_name.py
+++ tasks/get_operator_name.py
@@ -12,14 +12,15 @@ def get_operator_name(number, list_codes):
         return default_site_operator
     for i in range(0, len(list_codes)):
         temp_list = str(list_codes[i]).split(';')
+        # print(temp_list)
         if code == (temp_list[0].lstrip("['")):
             if int(temp_list[1]) <= int(num) <= int(temp_list[2]):
                 oper = temp_list[4]
                 if oper in config['oper_name']:
                     oper = config['oper_name'][oper]
                 return oper
-        else:
-            return default_site_operator
+        # else:
+        #     return default_site_operator
 
 
 def get_full_operator_name(number, list_codes):
