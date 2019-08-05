8f553b2d6a439b1e346a1015a6a5843d5050283b
Kirill Ivanov <ivanovkirillk@gmail.com>
2019-08-05 22:00:02 +0300

    user search in ad directory update

--- tasks/get_ad_user.py
+++ tasks/get_ad_user.py
@@ -1,15 +1,21 @@
 def get_ad_user(short_number, user_list):
     ad_user = None
     user_count = 0
+    found_list = []
     for user in user_list:
         temp_list = user.split('\t')
         if temp_list[0] == short_number:
             ad_user = temp_list[1].rstrip('\n')
             user_count += 1
+            found_list.append(ad_user)
         else:
             continue
-    if user_count != 1:
+    if user_count == 0:
         ad_user = None
-
+    elif user_count > 1:
+        for user in found_list:
+            if user[:3] == 'gpbu':
+                found_list.remove(user)
+        ad_user = found_list[0]
     return ad_user
 
